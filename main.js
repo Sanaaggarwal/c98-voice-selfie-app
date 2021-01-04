9eb150341142554b2f6da900f4619d44fc00ec06
Sanaaggarwal <72506943+Sanaaggarwal@users.noreply.github.com>
2021-01-04 16:51:56 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,3 +4,21 @@ function start(){
     document.getElementById("textbox").innerHTML="";
     recognition.start();
 }
+recognition.onresult=function(event){
+    console.log(event);
+    var content=event.results[0][0].transcript;
+    console.log(content);
+    document.getElementById("textbox").innerHTML=content;
+    speak();
+}
+function speak(){
+    var synth=window.speechSynthesis;
+    var speakdata=document.getElementById("textbox").value;
+    var utter=new SpeechSynthesisUtterance(speakdata);
+    synth.speak(utter);
+    Webcam.attach(camera);
+}
+Webcam.set({
+    width:350,height:300,image_format:'png',png_quality:100,dest_width:340,dest_height:290
+});
+camera=document.getElementById("camera");
