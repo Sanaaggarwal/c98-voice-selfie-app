78ee14d7daaad47921ac2f895029def8c74c161b
Sanaaggarwal <72506943+Sanaaggarwal@users.noreply.github.com>
2021-01-06 16:55:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -9,16 +9,37 @@ recognition.onresult=function(event){
     var content=event.results[0][0].transcript;
     console.log(content);
     document.getElementById("textbox").innerHTML=content;
-    speak();
+    if(content=="take my selfie"){
+     speak();
+     console.log("take the selfie");
+    }
+    else{
+        console.log("it will take the selfie when you will say take my selfie.");
+    }
 }
 function speak(){
     var synth=window.speechSynthesis;
-    var speakdata=document.getElementById("textbox").value;
+    var speakdata="taking you selfie in 3 seconds";
     var utter=new SpeechSynthesisUtterance(speakdata);
     synth.speak(utter);
     Webcam.attach(camera);
+setTimeout(function(){
+    takesnapshot();
+    save();
+},3000);
 }
 Webcam.set({
     width:350,height:300,image_format:'png',png_quality:100,dest_width:340,dest_height:290
 });
 camera=document.getElementById("camera");
+function takesnapshot(){
+    Webcam.snap(function(data_url){
+        document.getElementById("result").innerHTML='<img id="new_img" src="'+data_url+'">';
+    });
+}
+function save(){
+    link=document.getElementById("link");
+    image=document.getElementById("new_img").src;
+    link.href=image;
+    link.click();
+}
